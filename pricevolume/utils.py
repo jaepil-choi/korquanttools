67e36463c84f63ac0b7e50f4ce3e92d242714b39
jaepil-choi <chljeffreyz@gmail.com>
2021-12-01 23:49:56 +0900

    [REFACTOR] : Refactor date validation flow

--- pricevolume/utils.py
+++ pricevolume/utils.py
@@ -1,16 +1,27 @@
 from typing import Any, Dict, List, Optional, Union
+import pandas as pd
+
+def validate_date(yyyymmdd: Union[str, int], start="19900101", end="21000101") -> bool:
+    if not isinstance(yyyymmdd, [str, int]):
+        return False
+    
+    start_date = pd.to_datetime(start)
+    end_date = pd.to_datetime(end)
+
+    try:
+        date = pd.to_datetime(yyyymmdd)
+        return (start_date < date < end_date)
+    except:
+        return False
 
 def validate_date2str(yyyymmdd: Union[str, int]) -> str:
-    try:
-        if not (19900101 < int(yyyymmdd) < 21001231):
-            raise Exception("Wrong yyyymmdd range")
-        
-        yyyymmdd = str(yyyymmdd)
-    except ValueError as e:
-        raise Exception("Wrong type (ValueError)")
-    except Exception as e:
-        raise e
-    
-    return yyyymmdd
+    if validate_date(yyyymmdd):
+        return str(yyyymmdd)
+    else:
+        raise Exception("Date validation failed")
 
-    
+def validate_date2int(yyyymmdd: Union[str, int]) -> int:
+    if validate_date(yyyymmdd):
+        return int(yyyymmdd)
+    else:
+        raise Exception("Date validation failed")
